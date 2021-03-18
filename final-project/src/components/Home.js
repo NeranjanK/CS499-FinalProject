89a646e0921a1f50bb2738a178e0a44d23919ebd
NeranjanK <neranjan.kuppuswamy@gmail.com>
2021-03-17 23:24:42 -0700

    added styling for home and search page

--- final-project/src/components/Home.js
+++ final-project/src/components/Home.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    Route,
+    Switch,
+    Link,
+    NavLink,
+    Redirect,
+    useParams,
+    useRouteMatch, 
+    useLocation
+  } from 'react-router-dom';
+
+  import '../styles/home.css'
 
 function Home() {
     return (
-        <h1>Home</h1>
+        <div className = "home_box">
+            <div>
+                <div className = "title_holder">
+                    <h1 className = "title">Director's Cut</h1>
+
+                    <p className = "description">Your number one movie/tv show destination!</p>
+                </div>
+            </div>
+
+            <div className = "usage">
+                <Link to="/howtouse">How To Use</Link>
+            </div>
+        </div>
+
     );
 }
 
